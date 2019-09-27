f2efe4ee401bd87806fe633f1e1303db06ccf9ae
trunghv <trunghoang250394@gmail.com>
2019-09-27 17:33:35 +0700

    update config app

--- App.js
+++ App.js
@@ -7,64 +7,24 @@
  */
 
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import { Provider } from 'react-redux'
-import RootView from 'RootView'
 import store from 'middlewares/stores'
 import * as Sentry from '@sentry/react-native'
+import RootView from './src/RootView'
 Sentry.init({
   dsn: 'https://a12ca466e88843da82d0c48da3116a77@sentry.io/1763978'
 })
-function crashapp () {
-  Sentry.nativeCrash()
-  throw new Error('My first Sentry error!')
-}
+
 const App = () => {
-  crashapp()
   return (
     <Provider store={store}>
-      <RootView />
+      <RootView>
+
+      </RootView>
     </Provider>
   )
 }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
-  engine: {
-    position: 'absolute',
-    right: 0
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: '700'
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right'
-  }
-})
 
 export default App
---
+++ src/middlewares/actions/login/actionLogin.js
@@ -0,0 +1,7 @@
+import actionTypes from "../actionTypes"
+
+export const login =()=>{
+    return{
+        type:actionTypes.LOGIN
+    }
+}
